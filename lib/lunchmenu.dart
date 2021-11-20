fc27549490e927969f0a452f2a125e56f8f08b05
dksalsgus <alsgus2ek@naver.com>
2021-11-21 00:19:18 +0900

    update

--- lib/lunchmenu.dart
+++ lib/lunchmenu.dart
@@ -1,14 +1,12 @@
-import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LanchMenu extends StatefulWidget {
+class LunchMenu extends StatefulWidget {
   @override
-  State<LanchMenu> createState() => _LanchMenuState();
+  State<LunchMenu> createState() => _LunchMenuState();
 }
 
-class _LanchMenuState extends State<LanchMenu> {
+class _LunchMenuState extends State<LunchMenu> {
   final List<String> _menuList = [];
   final _addMenuController = TextEditingController();
 
@@ -24,7 +22,7 @@ class _LanchMenuState extends State<LanchMenu> {
       appBar: AppBar(
         title: const Text(
           '점심 메뉴 생성기',
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(color: Colors.amber),
         ),
         actions: [
           IconButton(
@@ -36,74 +34,9 @@ class _LanchMenuState extends State<LanchMenu> {
         width: double.infinity,
         height: double.infinity,
         color: Colors.amber,
-        child: _getBody(),
+        child: Text(_menuList.toString()),
       ),
     );
-  }
-
-  Widget _getBody() {
-    return Container(
-      width: double.infinity,
-      child: Column(
-        children: <Widget>[
-          const Text(
-            '추가한 메뉴',
-            style: TextStyle(fontSize: 40),
-          ),
-          const SizedBox(
-            height: 100,
-          ),
-          Text(
-            _menuList.toString(),
-            style: const TextStyle(fontSize: 40),
-          ),
-          const SizedBox(
-            height: 100,
-          ),
-          ElevatedButton(
-              onPressed: () => _selectButtonEvent(), child: const Text('뽑기')),
-          ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _menuList.clear();
-                });//
-              },
-              child: const Text('리셋'))
-        ],
-      ),
-      alignment: Alignment.center,
-    );
-  }
-
-  void _selectButtonEvent() {
-    if (_menuList.length > 2) {
-      // 메뉴가 있는 경우
-      _getRandomMenu();
-    } else {
-      showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('메뉴를 3개 이상 추가해주세요'),
-              actions: [
-                TextButton(
-                    onPressed: () => Navigator.pop(context), child: Text('OK'))
-              ],
-            );
-          });
-    }
-  }
-
-  void _getRandomMenu() {
-    Random _random = new Random();
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('오늘의 점심 메뉴는?'),
-            content: Text(_menuList[_random.nextInt(_menuList.length)]),
-          );
-        });
   }
 
   void _getBottomSheet() {
@@ -170,10 +103,7 @@ class _LanchMenuState extends State<LanchMenu> {
             Container(
                 margin: const EdgeInsets.all(10),
                 child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                      _addMenu(_addMenuController.text);
-                    },
+                    onPressed: () => _addMenu(_addMenuController.text),
                     child: const Text('추가하기')))
           ],
         ),
